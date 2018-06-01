e67b7544e606a47a1ea89fbe1bbced1e4e2e12f6
Kelvin Chan <2khc@users.noreply.github.com>
2018-06-01 10:25:38 +0100

    Refactor
    Turned into a class as opposed to just functions.
    Added methods to refresh the token

--- lib/api/reddit_api.dart
+++ lib/api/reddit_api.dart
@@ -1,70 +1,92 @@
-import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:http/http.dart' as http;
 import 'package:secure_string/secure_string.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'reddit_oauth_config.dart';
 
-Future<http.Response> fetchPost() async {
-  final _url = "http://www.reddit.com/r/android/hot";
-  print(_url);
-  final r = await http.get(_url);
-  final responseJson = json.decode(r.body);
-  print(r);
-  return http.get(_url);
-}
+class RedditOAuth {
+  RedditOAuthConfig _config;
+  String _basicHttpAuth;
+  String _bearerToken;
 
-void openWebView(BuildContext context) {
-  final RedditOAuthConfig config = new RedditOAuthConfig();
-  final SecureString secureString = new SecureString();
-  final String state = secureString.generate(length: 64);
-  final String authUrl =
-      "https://www.reddit.com/api/v1/authorize?client_id=${config.clientId}"
-      "&response_type=${config.responseType}"
-      "&state=$state&redirect_uri=${config.redirectUri}"
-      "&duration=${config.duration}&scope=identity,edit";
+  RedditOAuth(config) {
+    _config = config;
 
-  new MaterialApp(
-    routes: {
-      "/": (_) => new WebviewScaffold(
-            url: "http://www.google.com",
-            appBar: new AppBar(
-              title: new Text("Webview"),
-            ),
-          )
-    },
-  );
-  final webView = new FlutterWebviewPlugin();
-  webView.launch(authUrl,
-      rect: new Rect.fromLTWH(
-          0.0, 0.0, MediaQuery.of(context).size.width, 300.0));
+    final bytes = utf8.encode("${_config.clientId}:");
+    _basicHttpAuth = "basic ${base64.encode(bytes)}";
+  }
 
-  webView.onUrlChanged.listen((String url) {
-    List<String> urlComponents = url.split("/?");
+  void authorise(BuildContext context) {
+    final RedditOAuthConfig config = new RedditOAuthConfig();
+    final SecureString secureString = new SecureString();
+    final String state = secureString.generate(length: 64);
+    final String authUrl =
+        "https://www.reddit.com/api/v1/authorize?client_id=${config.clientId}"
+        "&response_type=${config.responseType}"
+        "&state=$state&redirect_uri=${config.redirectUri}"
+        "&duration=${config.duration}&scope=identity,edit";
 
-    if (urlComponents[0] == config.redirectUri) {
-      Map<String, String> params = Uri.splitQueryString(urlComponents[1]);
-      if (params["state"] == state) {
-        final bytes = utf8.encode("${config.clientId}:");
-        final String authHeader = "basic ${BASE64.encode(bytes)}";
-        http.post(
-          "https://www.reddit.com/api/v1/access_token",
-          headers: {"Authorization": authHeader},
-          body: {
-            "grant_type": "authorization_code",
-            "code": params["code"],
-            "redirect_uri": config.redirectUri
-          },
-        ).then((response) {
-        final body = response.body;
-          print(response.body);
+    new MaterialApp(
+      routes: {
+        "/": (_) => new WebviewScaffold(
+              url: "http://www.google.com",
+              appBar: new AppBar(
+                title: new Text("Webview"),
+              ),
+            )
+      },
+    );
+    final webView = new FlutterWebviewPlugin();
+    webView.launch(authUrl,
+        rect: new Rect.fromLTWH(
+            0.0, 0.0, MediaQuery.of(context).size.width, 300.0));
+
+    webView.onUrlChanged.listen((String url) {
+      List<String> urlComponents = url.split("/?");
+
+      if (urlComponents[0] == config.redirectUri) {
+        Map<String, String> params = Uri.splitQueryString(urlComponents[1]);
+        if (params["state"] == state) {
+          _retrieveToken(params["code"]);
           webView.close();
-        });
-        print("URL is: $url");
+        }
       }
-    }
-  });
+    });
+  }
+
+  void refreshToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String refreshToken = prefs.getString("refresh_token");
+    http.post(_config.tokenUrl, headers: {
+      "Authorization": _basicHttpAuth
+    }, body: {
+      "grant_type": "refresh_token",
+      "refresh_token": refreshToken
+    }).then((response) {
+      final Map<String, String> body = json.decode(response.body);
+      _bearerToken = body["access_token"];
+    });
+  }
+
+  void _retrieveToken(String code) {
+    http.post(
+      _config.tokenUrl,
+      headers: {"Authorization": _basicHttpAuth},
+      body: {
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": _config.redirectUri
+      },
+    ).then((response) async {
+      final Map<String, String> body = json.decode(response.body);
+      final prefs = await SharedPreferences.getInstance();
+      prefs.setString("refresh_token", body["refresh_token"]);
+      _bearerToken = body["access_token"];
+      print(response.body);
+    });
+  }
 }
--- lib/api/reddit_oauth_config.dart
+++ lib/api/reddit_oauth_config.dart
@@ -4,26 +4,11 @@ class RedditOAuthConfig {
   final String clientId = "5_xIeZY2TqCK-w";
   final String redirectUri = "http://localhost:8080";
   final String duration = "permanent";
-  final List<String> scope = [
-    "identity",
-    "edit",
-    "flair",
-    "history",
-    "modconfig",
-    "modflair",
-    "modlog",
-    "modposts",
-    "modwiki",
-    "mysubreddits",
-    "privatemessages",
-    "read",
-    "report",
-    "save",
-    "submit",
-    "subscribe",
-    "vote",
-    "wikiedit",
-    "wikiread"
-  ];
+  final String scope = "identity,edit,flair,history,modconfig,modflair,"
+      "modlog,modposts,modwiki,mysubreddits,privatemessages,read,report,save,"
+      "submit,subscribe,vote,wikiedit,wikiread";
+
   final String responseType = "code";
+
+  const RedditOAuthConfig();
 }
