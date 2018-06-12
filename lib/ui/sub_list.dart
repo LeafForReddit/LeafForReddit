6fa080c20cd09602fbd911610563a426aa44f64a
Jay <jay.jeyaruban.15@ucl.ac.uk>
2018-06-12 21:58:21 +0100

    Key elements viewable on sub list now.
    Identified key elements from listing payload relevant to sub list.
    Need to address title only being 1 line and spacing issue for right column.

--- lib/ui/sub_list.dart
+++ lib/ui/sub_list.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:leaf_for_reddit/api/reddit_client.dart';
 import 'package:leaf_for_reddit/official_themes.dart';
+import 'package:reddit/reddit.dart';
 import 'package:transparent_image/transparent_image.dart';
 
-
 class SubList extends StatefulWidget {
+  final RedditClient _client;
+
+  SubList(this._client);
+
   @override
   State<StatefulWidget> createState() {
     return new _SubListState();
@@ -13,54 +18,61 @@ class SubList extends StatefulWidget {
 class _SubListState extends State<SubList> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return new ListView(
-      children: <Widget>[
-        new FeedItem('Hello World'),
-        new FeedItem('Hello 2'),
-      ],
+    return new Container(
+      padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+      child: FutureBuilder<ListingResult>(
+        future: widget._client.fetchSubreddit('popular'),
+        builder: (context, snapshot) => new ListView(
+              children: snapshot.data['data']['children']
+                  .map<FeedItem>((child) => new FeedItem(child['data']))
+                  .toList(),
+            ),
+      ),
     );
   }
 }
 
 class FeedItem extends StatefulWidget {
-  final String uri;
+  final Map<String, dynamic> _listItem;
 
-  FeedItem(this.uri, {Key key}) : super(key: key);
+  FeedItem(this._listItem, {Key key}) : super(key: key);
 
   @override
   _FeedItemState createState() => new _FeedItemState();
 }
 
 class _FeedItemState extends State<FeedItem> {
-  String _heading = '';
-  int _voteCount = 0;
   int _votestatus = 0;
-  bool _isSwiped;
-
-  _FeedItemState() {
-    // TODO Perform logic for setting intial values for heading and votecount
-    this._heading = 'Hello';
-  }
+  bool _isSwiped = false;
 
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      child: new Row(
-        children: <Widget>[
-          new _UpvoteWidget(
-            voteCount: _voteCount,
-            userVoteStatus: _votestatus,
-            voteChange: newUserVote,
-          ),
-          new ThumbnailWidget(
-            imageUri: 'https://github.com/flutter/website/blob/master/_includes/code/layout/lakes/images/lake.jpg?raw=true',
-          ),
-          new Text(_heading),
-        ],
+    return new ConstrainedBox(
+      constraints: new BoxConstraints(
+        minHeight: 120.0,
       ),
-      padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-      height: 100.0,
+      child: new Container(
+        padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+        child: new Row(
+          children: <Widget>[
+            new _UpvoteWidget(
+              voteCount: widget._listItem['ups'],
+              userVoteStatus: _votestatus,
+              voteChange: newUserVote,
+            ),
+            new ThumbnailWidget(
+              imageUri: widget._listItem['thumbnail'],
+            ),
+            new TextBlockWidget(
+              widget._listItem['title'],
+              widget._listItem['subreddit_name_prefixed'],
+              widget._listItem['author'],
+              widget._listItem['created_utc'],
+              widget._listItem['num_comments'],
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -72,6 +84,64 @@ class _FeedItemState extends State<FeedItem> {
   }
 }
 
+class TextBlockWidget extends StatelessWidget {
+  final String _title;
+  final String _subreddit;
+  final String _poster;
+  final double _posted;
+  final int _commentNo;
+
+  TextBlockWidget(this._title, this._subreddit, this._poster, this._posted,
+      this._commentNo);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      verticalDirection: VerticalDirection.down,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        new Text(
+          '$_subreddit by $_poster',
+          style: TextStyle(
+            fontSize: 10.0,
+            color: Colors.grey,
+          ),
+          overflow: TextOverflow.ellipsis,
+        ),
+        new Container(
+          child: new Text(
+            _title,
+            style: TextStyle(
+              fontSize: 16.0,
+            ),
+            softWrap: true,
+            maxLines: 3,
+          ),
+        ),
+        new Text(
+          '${_commentNo.toString()} comments * ${_calcElapsedTime(_posted)}',
+          style: TextStyle(
+            fontSize: 10.0,
+            color: Colors.grey,
+            fontStyle: FontStyle.italic,
+          ),
+        )
+      ],
+    );
+  }
+
+  static String _calcElapsedTime(double epocPostTime) {
+    int diff = new DateTime.now()
+        .difference(new DateTime.fromMillisecondsSinceEpoch(
+            (epocPostTime * 1000).floor(),
+            isUtc: true))
+        .inHours;
+
+    return '${diff.toString()} ${(diff == 1) ? 'hour' : 'hours'} ago';
+  }
+}
 
 class ThumbnailWidget extends StatelessWidget {
   final String imageUri;
@@ -85,11 +155,10 @@ class ThumbnailWidget extends StatelessWidget {
         placeholder: kTransparentImage,
         image: imageUri,
         fit: BoxFit.cover,
-        height: 70.0,
-        width: 70.0,
+        height: 88.0,
+        width: 88.0,
       ),
-      padding: const EdgeInsets.only(
-          left: 5.0, right: 5.0),
+      padding: const EdgeInsets.only(left: 5.0, right: 5.0),
     );
   }
 }
@@ -101,7 +170,9 @@ class _UpvoteWidget extends StatelessWidget {
   final ValueChanged<int> voteChange;
 
   _UpvoteWidget(
-      {@required this.voteCount, @required this.userVoteStatus, @required this.voteChange});
+      {@required this.voteCount,
+      @required this.userVoteStatus,
+      @required this.voteChange});
 
   void _votePress(int status) {
     var newStatus = (status == userVoteStatus) ? 0 : status;
@@ -114,28 +185,32 @@ class _UpvoteWidget extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
         new GestureDetector(
-          child: new Icon(Icons.keyboard_arrow_up,
-            color: (userVoteStatus > 0)
-                ? LeafColors.upvote_color
-                : null,),
+          child: new Icon(
+            Icons.keyboard_arrow_up,
+            color: (userVoteStatus > 0) ? LeafColors.upvote_color : null,
+          ),
           onTap: () => _votePress(1),
         ),
-        new Text(voteCount.toString(),
+        new Text(
+          _formatUps(voteCount),
           style: new TextStyle(
-            color: (userVoteStatus > 0) ? LeafColors.upvote_color : ((userVoteStatus < 0)
-                ? LeafColors.downvote_color
-                : null),
+            color: (userVoteStatus > 0)
+                ? LeafColors.upvote_color
+                : ((userVoteStatus < 0) ? LeafColors.downvote_color : null),
           ),
         ),
         new GestureDetector(
-          child: new Icon(Icons.keyboard_arrow_down,
-            color: (userVoteStatus < 0)
-                ? LeafColors.downvote_color
-                : null,
+          child: new Icon(
+            Icons.keyboard_arrow_down,
+            color: (userVoteStatus < 0) ? LeafColors.downvote_color : null,
           ),
           onTap: () => _votePress(-1),
         ),
       ],
     );
   }
+
+  //TODO Consider use of bloc for this element and moving this method there
+  static String _formatUps(int ups) =>
+      (ups < 1000) ? ups.toString() : '${(ups / 1000).floor()}k';
 }
