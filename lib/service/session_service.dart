388d023d50e2d1e992ad214caedb5153a54b0344
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2018-09-05 15:42:43 +0100

    Refactored based on user profile view

--- lib/service/session_service.dart
+++ lib/service/session_service.dart
@@ -4,10 +4,19 @@ import 'package:rxdart/subjects.dart';
 import 'package:uuid/uuid.dart';
 
 class SessionService {
-  final _currentSubredditSubject = new BehaviorSubject<String>(seedValue: 'Popular');
-  final _currentDeviceIdSubject = new BehaviorSubject<String>(seedValue: new Uuid().v4());
+  final _currentSubredditSubject =
+      new BehaviorSubject<String>(seedValue: 'Popular');
+  final _currentUserSubject =
+      new BehaviorSubject<User>(seedValue: new User('Text', new Uuid().v4()));
 
   Stream<String> get currentSubreddit => _currentSubredditSubject.stream;
 
-  Stream<String> get currentDeviceId => _currentDeviceIdSubject.stream;
+  Stream<User> get currentUser => _currentUserSubject.stream;
+}
+
+class User {
+  final String username;
+  final String deviceId;
+
+  User(this.username, this.deviceId);
 }
